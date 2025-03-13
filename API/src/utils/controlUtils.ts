01b2ef72dc65c197b95ef33e12d0178f53b94ce2
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2025-03-13 14:20:16 +0800

    feat: add filter support
    api calls now support filtering in finding
    large number of items

--- API/src/utils/controlUtils.ts
+++ API/src/utils/controlUtils.ts
@@ -1,47 +1,40 @@
 import { ErrorRequestHandler } from "express";
-import { middlewareFactory } from "../../types";
+import { queryParser } from "../../types";
 
 //middleware to parse queries for data
-const parseQueries: middlewareFactory = (fields) => (req, res, next) => {
+const parseQueries: queryParser = (fields) => (req, res, next) => {
   try {
-    let { sort = "-datetime", limit = "15", offset = "0" } = req.query;
-    if (
-      typeof sort !== "string" ||
-      typeof limit != "string" ||
-      typeof offset !== "string"
-    ) {
-      res.status(400).end();
-      return;
+    for(let field in req.query){
+      if (typeof req.query[field] !== "string") {
+        res.status(400).end();
+        return;
+      }
     }
 
+    let { sort = "-datetime", limit = "15", offset = "0" } = req.query as Record<string, string>;
     //prepares object to be referenced by orderBy arg in prisma
     //e.g +username orders usernames in ascending order
     //-datetime orders datetime in descending order
-    let orderByObj: { [key: string]: string } = {};
+    let orderByList: {[key:string]:string}[] = [];
     sort.split(",").forEach((str) => {
-      let order = "";
-      switch (str[0]) {
-        case "+":
-          order = "asc";
-          break;
-        case "-":
-          order = "desc";
-          break;
-        default:
-          res.status(400).end();
-          return;
-      }
+      const order = str[0] === "-" ? "desc" : "asc";
       const field = str.slice(1);
-
-      if (fields.filter((currField) => field === currField).length === 0) {
-        res.status(400).end();
-      }
-      orderByObj[field] = order;
+      if (fields.includes(field)) orderByList.push({[field]:order});
     });
+
+    //example: ?fieldName1=1224513&fieldName2=1626
+    let filterByObj: { [key: string]: string } = {};
+    fields.forEach((field) => {
+      if (field in req.query) {
+        filterByObj[field] = req.query[field] as string;
+      }
+    });
+    console.log(filterByObj);
 
     req.body.skip = parseInt(offset);
     req.body.take = parseInt(limit);
-    req.body.orderBy = orderByObj;
+    req.body.orderBy = orderByList;
+    req.body.where = filterByObj;
     next();
   } catch (error) {
     next(error);
@@ -49,11 +42,20 @@ const parseQueries: middlewareFactory = (fields) => (req, res, next) => {
 };
 
 const handleError: ErrorRequestHandler = (err, req, res, next) => {
-  if(err.code === "P2002"){
-    res.status(400).end();
-  }
-    console.error(err);
-    res.status(500).end();
-}
+  //uniqueness constraint failed
+  if (err.code === "P2002") {
+    const errFields = err.meta.target.join(",");
 
-export {parseQueries, handleError};
+    const message = `uniqueness constraint failed in ${errFields}`;
+    res.status(400).json({ message });
+  }
+  //An operation failed because it depends on one or more records that were required but not found
+  if (err.code === "P2025") {
+    const message = err.meta.cause;
+    res.status(404).json({ message });
+  }
+  console.error(err);
+  res.status(500).end();
+};
+
+export { parseQueries, handleError };
